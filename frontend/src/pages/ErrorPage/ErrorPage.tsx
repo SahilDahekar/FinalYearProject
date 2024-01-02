5c64bb2f9a6795ccf5456db82deb2dd20a0c0362
SahilDahekar <dahekar30sahil@gmail.com>
2024-01-02 23:53:01 +0530

    Styled default ErrorPage

--- frontend/src/pages/ErrorPage/ErrorPage.tsx
+++ frontend/src/pages/ErrorPage/ErrorPage.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button"
+import { Link } from "react-router-dom"
 
 function ErrorPage() {
   return (
-    <div>Default 404 page</div>
+    <div className='grid place-content-center gap-4 h-screen place-items-center'>
+      <h1 className='text-4xl font-semibold tracking-tight'>Oops...</h1>
+      <p className='text-xl '>Seems like you are trying to find a page that does not exist</p>
+      <Button asChild>
+        <Link to='/'>Go Back</Link>
+      </Button>
+    </div>
   )
 }
 

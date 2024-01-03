5ad27636938df04064cba19640de083b9274f81f
SahilDahekar <dahekar30sahil@gmail.com>
2024-01-04 01:59:28 +0530

    Added cors to allow frontend requests

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,19 @@
 import  express  from "express";
-import { config} from "dotenv";
+import { config } from "dotenv";
 config();
+import cors from "cors";
 import morgan from 'morgan'
 import appRouter from "./routes/index.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
+// Configure CORS to allow only requests from port 5173
+const corsOptions = {
+    origin: 'http://localhost:5173',  // Replace with your specific origin
+    optionsSuccessStatus: 200,
+};
+  
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));

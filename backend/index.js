58a07e94b2090996c5d52f456b886da664567aa7
Prathamesh-V-Kirad <kiradvprathamesh2002@gamil.com>
2024-01-09 12:40:51 +0530

    Added websocket.js file and modified studio.tsx

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 
 import app from './app.js'
 import { connectToDatabase } from './db/connections.js'
+import WebSocket from './utils/websocket.js';
 
+WebSocket();
 connectToDatabase()
     .then(() => {
         app.listen(8000, () => console.log("server started and connected to db at port 8000"));
--- backend/models/schema.js
+++ backend/models/schema.js
@@ -10,8 +10,8 @@ const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
-  destinationSchema:[destinationSchema],
-  broadcastSchema:[broadcastSchema],
+  //destinationSchema:[destinationSchema],
+  //broadcastSchema:[broadcastSchema],
 },{
     timestamp: true,
 });

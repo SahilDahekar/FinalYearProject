73bfd08f01f1d50675000e8bfed316593a0e14cd
Prathamesh-V-Kirad <kiradvprathamesh2002@gamil.com>
2024-01-16 22:20:53 +0530

    added config and mediasoup

---
+++ backend/utils/worker.js
@@ -0,0 +1,35 @@
+import * as mediasoup from 'mediasoup';
+import config  from './soup_config.js';
+//import {Worker , Router} from "mediasoup/node/lib/types";
+
+// const worker : Array <{
+//     worker:Worker
+//     router:Router
+// }>=[];
+let worker;
+
+let nextMediasoupWorkerIdx = 0;
+const createWorker = async() =>{
+    console.log("created");
+    try{
+        worker = await mediasoup.createWorker({
+        logLevel: config.mediasoup.worker.logLevel,
+        logTags: config.mediasoup.worker.logTags,
+        rtcMinPort: config.mediasoup.worker.rtcMinPort,
+        rtcMaxPort: config.mediasoup.worker.rtcMaxPort,    
+    });
+    }catch(err){
+        throw err;
+    }
+    worker.on('died',()=>{
+        console.error(`mediasoup worker died , [${worker.pid}]`);
+        setTimeout(()=>{
+            process.exit(1);
+        },2000);
+    })
+    const mediaCodecs = config.mediasoup.router.mediaCodecs;
+    const mediasoupRouter = await worker.createRouter({mediaCodecs});
+    return mediasoupRouter;
+}
+
+export { createWorker };

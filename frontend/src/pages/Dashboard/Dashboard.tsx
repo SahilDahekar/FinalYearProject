a6501f5984b2fcf2fe7f547b6d499ee40294df82
SahilDahekar <dahekar30sahil@gmail.com>
2024-01-28 23:40:34 +0530

    Added toast for logout and moved to seperate function

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -5,26 +5,23 @@ import { IoVideocam ,IoKey } from "react-icons/io5";
 import { TbLogout2 } from "react-icons/tb";
 import { useAuth } from '@/context/AuthContext';
 import { useNavigate } from "react-router-dom";
-import { Toaster } from "@/components/ui/toaster"
+import { useToast } from '@/components/ui/use-toast';
 
 function Dashboard() {
     const auth = useAuth();
     const navigate = useNavigate();
-    
-    if(auth?.isLoading){
-        return <div>Loading..</div>;
+    const { toast } = useToast();
+
+    function handleLogout(){
+        auth?.logout();
+        navigate('/');
+        toast({
+            title : `Logged out ${auth?.user?.name}`
+        })
     }
-    
-    // if (!auth?.user) {
-    //     if (auth?.isLoading) {
-    //         return <div>Loading..</div>;
-    //     }
-    //     return <Navigate to="/" />;
-    // }
 
     return (
         <div className='h-screen'>
-            <Toaster/>
             <div className='flex justify-between py-4 px-3 border-b'>
                 <h1 className='text-3xl font-semibold tracking-tight'>Dashboard</h1>
                 <div>
@@ -42,10 +39,7 @@ function Dashboard() {
                     <Button className='text-base' asChild variant="ghost" size='lg'>
                         <Link to="/destination"><IoKey className='mr-2'/>Destination</Link>
                     </Button>
-                    <Button onClick={() => {
-                        auth?.logout();
-                        navigate('/');
-                    }} className='text-base mt-auto' variant="default" size='lg'>
+                    <Button onClick={handleLogout} className='text-base mt-auto' variant="default" size='lg'>
                         <TbLogout2 className='mr-2'/> Sign Out
                     </Button>
                 </div>

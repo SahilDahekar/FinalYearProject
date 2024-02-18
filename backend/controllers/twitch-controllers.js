e1dfce67a705453970ca42c2f925014a11c228ca
SahilDahekar <dahekar30sahil@gmail.com>
2024-02-18 23:49:54 +0530

    Added function templates to start and stop twitch broadcast

---
+++ backend/controllers/twitch-controllers.js
@@ -0,0 +1,22 @@
+
+export const startTwitchBroadcast = async () => {
+    try {
+        
+    } catch (error) {
+        return {
+            message : "error",
+            error : error
+        }
+    }
+}
+
+export const stopTwitchBroadcast = async () => {
+    try {
+        
+    } catch (error) {
+        return {
+            message : "error",
+            error : error
+        }
+    }
+}

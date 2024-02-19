a4cec369d9ced91459b1e9839c0bdb026fa7eb23
SahilDahekar <dahekar30sahil@gmail.com>
2024-02-19 22:27:18 +0530

    Added logic to start twitch broadcast

--- backend/controllers/twitch-controllers.js
+++ backend/controllers/twitch-controllers.js
@@ -1,7 +1,21 @@
+import axios from "axios";
 
-export const startTwitchBroadcast = async () => {
+export const startTwitchBroadcast = async (params) => {
+
+    // Send your stream to one of these ingest servers https://dev.twitch.tv/docs/video-broadcast/reference/#get-ingest-servers
+
+    // GET https://ingest.twitch.tv/ingests
+
     try {
-        
+        const { data, config } = params;
+        const response = await axios.patch(
+            `https://api.twitch.tv/helix/channels?broadcaster_id=${data.twitchUserId}`,
+            data.title,
+            config
+        );
+
+        return response.data;
+
     } catch (error) {
         return {
             message : "error",
@@ -12,7 +26,7 @@ export const startTwitchBroadcast = async () => {
 
 export const stopTwitchBroadcast = async () => {
     try {
-        
+
     } catch (error) {
         return {
             message : "error",

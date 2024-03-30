bf7467fa1081aba0118321b8b9aac0db1e093eb6
SahilDahekar <dahekar30sahil@gmail.com>
2024-03-31 00:12:52 +0530

    Added name form to studiomodal

--- frontend/src/components/StudioModal/StudioModal.tsx
+++ frontend/src/components/StudioModal/StudioModal.tsx
@@ -1,27 +1,75 @@
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle
 } from "@/components/ui/dialog";
-import { Form } from "../ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import { Input } from "../ui/input";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "../ui/button";
 
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: "Name must be at least 2 characters.",
+  }),
+});
 
 function StudioModal() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const name = values.name;
+    try {
+      // do something here
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <Dialog defaultOpen>
-        <DialogContent>
-            <DialogHeader>
-            <DialogTitle>User Details</DialogTitle>
-            <DialogDescription>
-                Please specify the stream details
-            </DialogDescription>
-            <div>
-              
-            </div>
-            </DialogHeader>
-        </DialogContent>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>User Details</DialogTitle>
+          <DialogDescription>
+            Please specify your stream details
+          </DialogDescription>
+          <div>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="flex flex-col space-y-4"
+              >
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Name</FormLabel>
+                      <FormControl>
+                        <Input placeholder="John Doe" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit" className="">
+                  Submit
+                </Button>
+              </form>
+            </Form>
+          </div>
+        </DialogHeader>
+      </DialogContent>
     </Dialog>
   )
 }
